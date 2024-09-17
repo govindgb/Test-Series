3f46a90f0daffd90cbded19d2c32694f764d1e05
govind <govind@fathershops.com>
2024-09-17 17:44:16 +0530

    test sereis initialization with adding taiwind done

---
+++ test-series-app/pages/footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+ const footer = () => {
+  return (
+    <>
+    <div>footer</div>
+    <p>here we start writing footer frontend code</p>
+    </>
+  )
+}
+
+export default footer;
